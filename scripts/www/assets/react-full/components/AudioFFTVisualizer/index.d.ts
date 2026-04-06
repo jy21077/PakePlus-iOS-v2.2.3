aa3c5a4ac71b82d6513a632ef927ef5cd55ac3bd
jy21077 <168295655+jy21077@users.noreply.github.com>
2026-04-06 16:28:40 +0800

    update src file from pakeplus

---
+++ scripts/www/assets/react-full/components/AudioFFTVisualizer/index.d.ts
@@ -0,0 +1,5 @@
+import { FC, HTMLProps } from 'react';
+export declare const AudioFFTVisualizer: FC<{
+    fftData: number[];
+} & HTMLProps<HTMLCanvasElement>>;
+//# sourceMappingURL=index.d.ts.map
