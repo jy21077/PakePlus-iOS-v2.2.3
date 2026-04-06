2c2cd79b742cf89996031601258b1523e151e6cd
jy21077 <168295655+jy21077@users.noreply.github.com>
2026-04-06 16:30:02 +0800

    update src file from pakeplus

---
+++ scripts/www/assets/react-full/layout/horizontal.d.ts
@@ -0,0 +1,12 @@
+import { default as React, HTMLProps } from 'react';
+export declare const HorizontalLayout: React.FC<{
+    thumbSlot?: React.ReactNode;
+    coverSlot?: React.ReactNode;
+    controlsSlot?: React.ReactNode;
+    lyricSlot?: React.ReactNode;
+    backgroundSlot?: React.ReactNode;
+    bottomControls?: React.ReactNode;
+    hideLyric?: boolean;
+    asChild?: boolean;
+} & HTMLProps<HTMLDivElement>>;
+//# sourceMappingURL=horizontal.d.ts.map
