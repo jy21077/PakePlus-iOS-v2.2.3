9d8db12d88791f50c04567698a36837d94db289a
jy21077 <168295655+jy21077@users.noreply.github.com>
2026-04-06 16:29:22 +0800

    update src file from pakeplus

---
+++ scripts/www/assets/react-full/components/MusicInfo/index.d.ts
@@ -0,0 +1,10 @@
+import { HTMLProps } from 'react';
+export declare const MusicInfo: React.FC<{
+    name?: string;
+    artists?: string[];
+    album?: string;
+    onArtistClicked?: (artist: string, index: number) => void;
+    onAlbumClicked?: () => void;
+    onMenuButtonClicked?: () => void;
+} & HTMLProps<HTMLDivElement>>;
+//# sourceMappingURL=index.d.ts.map
