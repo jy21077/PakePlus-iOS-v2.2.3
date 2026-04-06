32c8875ca182c62a1f65a48f9322a68ff62a8cdd
jy21077 <168295655+jy21077@users.noreply.github.com>
2026-04-06 16:28:45 +0800

    update src file from pakeplus

---
+++ scripts/www/assets/react-full/components/AudioQualityTag/index.d.ts
@@ -0,0 +1,7 @@
+import { HTMLProps } from 'react';
+export declare const AudioQualityTag: import('react').NamedExoticComponent<Omit<{
+    isDolbyAtmos?: boolean;
+    tagText?: string;
+    tagIcon?: boolean;
+} & HTMLProps<HTMLDivElement>, "ref"> & import('react').RefAttributes<HTMLDivElement>>;
+//# sourceMappingURL=index.d.ts.map
