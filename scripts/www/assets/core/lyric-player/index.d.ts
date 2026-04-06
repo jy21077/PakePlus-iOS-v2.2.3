dbecf1d135a2c16e87dddd9eb80b5fc5b4a20d34
jy21077 <168295655+jy21077@users.noreply.github.com>
2026-04-06 16:26:38 +0800

    update src file from pakeplus

---
+++ scripts/www/assets/core/lyric-player/index.d.ts
@@ -0,0 +1,20 @@
+import { DomLyricPlayer } from './dom/index.ts';
+export { LyricPlayerBase } from './base.ts';
+export * from './canvas/index.ts';
+export * from './dom-slim/index.ts';
+export * from './dom/index.ts';
+/**
+ * 歌词中不雅用语的掩码模式
+ */
+declare enum MaskObsceneWordsMode {
+    /** 禁用任何不雅用语掩码 */
+    Disabled = "",
+    /** 完全掩码所有不雅用语 */
+    FullMask = "full-mask"
+}
+export { MaskObsceneWordsMode, 
+/**
+ * 默认导出的歌词播放组件
+ */
+DomLyricPlayer as LyricPlayer, };
+//# sourceMappingURL=index.d.ts.map
