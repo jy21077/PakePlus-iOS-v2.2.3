5c228b8a7dcd9f2095b879bc1c0983de25b28c83
jy21077 <168295655+jy21077@users.noreply.github.com>
2026-04-06 16:27:31 +0800

    update src file from pakeplus

---
+++ scripts/www/assets/core/utils/optimize-lyric.d.ts
@@ -0,0 +1,9 @@
+import { LyricLine } from '../interfaces.ts';
+/**
+ * 优化歌词行的展示效果
+ *
+ * 注意会直接原地修改入参，确保你已经提前深克隆了歌词行数组
+ * @param lines 歌词行数组
+ */
+export declare function optimizeLyricLines(lines: LyricLine[]): void;
+//# sourceMappingURL=optimize-lyric.d.ts.map
