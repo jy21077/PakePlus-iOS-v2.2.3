3bed68c2a775e73fc7f242cdbef027d9024f8961
jy21077 <168295655+jy21077@users.noreply.github.com>
2026-04-06 16:29:42 +0800

    update src file from pakeplus

---
+++ scripts/www/assets/react-full/components/ToggleIconButton/prebuilt-enum.d.ts
@@ -0,0 +1,9 @@
+export declare enum PrebuiltToggleIconButtonType {
+    Lyrics = "lyrics",
+    Playlist = "playlist",
+    Repeat = "repeat",
+    Shuffle = "shuffle",
+    Star = "star",
+    AirPlay = "airplay"
+}
+//# sourceMappingURL=prebuilt-enum.d.ts.map
