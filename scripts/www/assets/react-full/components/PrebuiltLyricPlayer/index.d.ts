4bd697c2cf47ada2a506b56cafa67e39c8f1d1fa
jy21077 <168295655+jy21077@users.noreply.github.com>
2026-04-06 16:29:27 +0800

    update src file from pakeplus

---
+++ scripts/www/assets/react-full/components/PrebuiltLyricPlayer/index.d.ts
@@ -0,0 +1,6 @@
+import { FC, HTMLProps } from 'react';
+/**
+ * 已经部署好所有组件的歌词播放器组件，在正确设置所有的 Jotai 状态后可以开箱即用
+ */
+export declare const PrebuiltLyricPlayer: FC<HTMLProps<HTMLDivElement>>;
+//# sourceMappingURL=index.d.ts.map
