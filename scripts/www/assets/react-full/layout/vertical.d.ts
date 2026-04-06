76fbec8086b8c2b962992b6cb6e4b5554843c7c8
jy21077 <168295655+jy21077@users.noreply.github.com>
2026-04-06 16:30:13 +0800

    update src file from pakeplus

---
+++ scripts/www/assets/react-full/layout/vertical.d.ts
@@ -0,0 +1,12 @@
+import { default as React, HTMLProps } from 'react';
+export declare const VerticalLayout: React.FC<{
+    thumbSlot?: React.ReactNode;
+    smallControlsSlot?: React.ReactNode;
+    bigControlsSlot?: React.ReactNode;
+    coverSlot?: React.ReactNode;
+    lyricSlot?: React.ReactNode;
+    asChild?: boolean;
+    hideLyric?: boolean;
+    immerseCover?: boolean;
+} & HTMLProps<HTMLDivElement>>;
+//# sourceMappingURL=vertical.d.ts.map
