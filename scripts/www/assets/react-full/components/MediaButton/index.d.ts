ed9416baee1b4e1bd01c25a40658ba0983c4edbb
jy21077 <168295655+jy21077@users.noreply.github.com>
2026-04-06 16:29:13 +0800

    update src file from pakeplus

---
+++ scripts/www/assets/react-full/components/MediaButton/index.d.ts
@@ -0,0 +1,3 @@
+import { FC, HTMLProps, PropsWithChildren } from 'react';
+export declare const MediaButton: FC<PropsWithChildren<HTMLProps<HTMLButtonElement>>>;
+//# sourceMappingURL=index.d.ts.map
