24a2a3f72b1a3146da50c326ec473599a1154fac
jy21077 <168295655+jy21077@users.noreply.github.com>
2026-04-06 16:29:47 +0800

    update src file from pakeplus

---
+++ scripts/www/assets/react-full/components/VolumeControlSlider/index.d.ts
@@ -0,0 +1,4 @@
+import { default as React } from 'react';
+import { SliderProps } from '../BouncingSlider';
+export declare const VolumeControl: React.FC<SliderProps>;
+//# sourceMappingURL=index.d.ts.map
