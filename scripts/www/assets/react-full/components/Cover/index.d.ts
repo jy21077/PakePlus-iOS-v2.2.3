aed0ed6110670ab83431f8ba5a1ce8c359090a32
jy21077 <168295655+jy21077@users.noreply.github.com>
2026-04-06 16:29:01 +0800

    update src file from pakeplus

---
+++ scripts/www/assets/react-full/components/Cover/index.d.ts
@@ -0,0 +1,12 @@
+import { HTMLProps } from 'react';
+/**
+ * 一个专辑图组件
+ */
+export declare const Cover: import('react').ForwardRefExoticComponent<Omit<{
+    coverUrl?: string;
+    coverIsVideo?: boolean;
+    coverVideoPaused?: boolean;
+    musicPaused?: boolean;
+    pauseShrinkAspect?: number;
+} & HTMLProps<HTMLElement>, "ref"> & import('react').RefAttributes<HTMLDivElement>>;
+//# sourceMappingURL=index.d.ts.map
